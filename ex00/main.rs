1c65fa675aee55949b0aa60ef3a7933fcc119f24
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-13 20:09:45 +0100

    add Matrix

--- ex00/main.rs
+++ ex00/main.rs
@@ -1,20 +1,53 @@
-#[derive(Debug, Clone)]
-struct Vector<K> {
-    data: Vec<K>,
-}
-
-// #[derive(Debug, Clone)]
-// struct Matrix<K> {
-//     data: Vec<K>,
-//     rows: usize,
-//     cols: usize,
-// }
-
 use std::{
     ops::{Add, Sub, Mul},
     fmt::Debug,
 };
 
+#[derive(Debug, Clone)]
+struct Vector<K> {
+    data: Vec<K>,
+}
+
+#[derive(Debug, Clone)]
+struct Matrix<K> {
+    data: Vec<Vec<K>>,
+    rows: usize,
+    cols: usize,
+}
+
+impl<K> Matrix<K>
+where
+    K: Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Copy + Debug,
+{
+    fn from<T : Into<Vec<Vec<K>>>>(values: T) -> Self{
+        let data = values.into();
+        let rows = data.len();
+        let cols = if rows > 0 { data[0].len() } else { 0 };
+        Matrix {data, rows, cols}
+    }
+
+    fn add(&mut self, v: &Matrix<K>){
+        for i in 0..self.data.len(){
+            for j in 0..self.data[i].len(){
+                self.data[i][j] = self.data[i][j] + v.data[i][j];
+            }
+        }
+    }
+    fn sub(&mut self, v: &Matrix<K>){
+        for i in 0..self.data.len(){
+            for j in 0..self.data[i].len(){
+                self.data[i][j] = self.data[i][j] - v.data[i][j];
+            }
+        }
+    }
+    fn scl(&mut self, a: K){
+        for i in 0..self.data.len(){
+            for j in 0..self.data[i].len(){
+                self.data[i][j] = self.data[i][j] * a;
+            }
+        }
+    }
+}
 
 impl<K> Vector<K> 
 where
@@ -77,4 +110,20 @@ fn main() {
     j.print();
     j.scl(5.);
     j.print();
+
+
+    let mut u1 = Matrix::from(vec![vec![1., 2.], vec![3., 4.]]);
+    let mut u2 = Matrix::from(vec![vec![1., 0.], vec![8., 7.]]);
+    let mut u3 = Matrix::from(vec![vec![1., 0.], vec![8., 7.]]);
+    let v1 = Matrix::from(vec![vec![7., 4.], vec![-2., 2.]]);
+    u1.add(&v1);
+    u2.sub(&v1);
+    u3.scl(-1.);
+    println!("Rows: {}, Cols: {}", u1.rows, u1.cols);
+    println!("Data: {:?}", u1.data);
+    println!("=================");
+    println!("Data: {:?}", u2.data);
+    println!("=================");
+    println!("Data: {:?}", u3.data);
+
 }
