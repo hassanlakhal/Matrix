c578821eb81c21838bf348cce7e14c01eba58b6f
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-13 15:04:30 +0100

    add sub method

--- ex00/main.rs
+++ ex00/main.rs
@@ -10,44 +10,65 @@ struct Vector<K> {
 //     cols: usize,
 // }
 
-use std::ops::Add;
+use std::{
+    ops::{Add, Sub},
+    fmt::Debug,
+};
 
-impl<K: Add<Output = K> + Copy + std::fmt::Debug> Vector<K> {
-    fn from<T: Into<Vec<K>>>(values: T) -> Self {
-        Vector { data: values.into() }
-    }
 
-    fn size(&self) -> usize {
-        self.data.len()
-    }
-
-    fn print(&self) {
-        println!("{:?}", self.data);
-    }
-
-    fn add(&mut self, v: &Vector<K>) {
-        if self.size() != v.size() {
-            eprintln!("Error: Vectors must have the same size");
-            return;
+impl<K> Vector<K> 
+where
+    K: Add<Output = K> + Sub<Output = K> + Copy + Debug,
+{
+        fn from<T: Into<Vec<K>>>(values: T) -> Self {
+            Vector { data: values.into() }
         }
-        for i in 0..self.data.len() {
-            self.data[i] = self.data[i] + v.data[i];
-        }
-    }
 
-    // fn sub...
+        fn size(&self) -> usize {
+            self.data.len()
+        }
+
+        fn print(&self) {
+            println!("{:?}", self.data);
+        }
+
+        fn add(&mut self, v: &Vector<K>) {
+            if self.size() != v.size() {
+                eprintln!("Error: Vectors must have the same size");
+                return;
+            }
+            for i in 0..self.data.len() {
+                self.data[i] = self.data[i] + v.data[i];
+            }
+        }
+
+        fn sub(&mut self, v: &Vector<K>){
+            if self.size() != v.size() {
+                eprintln!("Error: Vectors must have the same size");
+                return;
+            }
+            for i in 0..self.data.len() {
+                self.data[i] = self.data[i] - v.data[i];
+            }
+        }
     // fn scl...
 }
 
 fn main() {
     let mut u = Vector::from([2., 3.]);
     let v = Vector::from([5., 7.]);
+    let mut j = Vector::from([8., 9.]);
 
     println!("Before add:");
     u.print();  
 
     u.add(&v);
 
+
     println!("After add:");
     u.print();
+
+    j.sub(&v);
+    println!("After sub:");
+    j.print();
 }
