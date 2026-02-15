761cd4d1cd0102c563b8944a08027d37bad44249
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-15 23:20:03 +0000

    add Display

--- ex02/main.rs
+++ ex02/main.rs
@@ -1,7 +1,6 @@
-use std::{
-    fmt::Debug,
-    ops::{Add, Mul, Sub},
-};
+use std::fmt;
+use std::ops::{AddAssign, SubAssign, MulAssign};
+
 
 #[derive(Debug, Clone)]
 struct Vector<K> {
@@ -10,116 +9,131 @@ struct Vector<K> {
 
 #[derive(Debug, Clone)]
 struct Matrix<K> {
-    data: Vec<Vec<K>>,
+    data: Vec<Vec<K>>, // Column-major storage
     rows: usize,
     cols: usize,
 }
 
-trait Scalar: Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Copy + Debug {}
-impl<T> Scalar for T where T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Copy + Debug {}
 
-impl<K: Scalar> Matrix<K> {
-    fn from(data: Vec<Vec<K>>) -> Self {
-        let rows = data.len();
-        let cols = if rows > 0 { data[0].len() } else { 0 };
-        Matrix { data, rows, cols }
-    }
-
-    fn add(&mut self, v: &Matrix<K>) {
-        for i in 0..self.rows {
-            for j in 0..self.cols {
-                self.data[i][j] = self.data[i][j] + v.data[i][j];
-            }
-        }
-    }
-
-    fn sub(&mut self, v: &Matrix<K>) {
-        for i in 0..self.rows {
-            for j in 0..self.cols {
-                self.data[i][j] = self.data[i][j] - v.data[i][j];
-            }
-        }
-    }
-
-    fn scl(&mut self, a: K) {
-        for i in 0..self.rows {
-            for j in 0..self.cols {
-                self.data[i][j] = self.data[i][j] * a;
-            }
-        }
-    }
-}
-
-impl<K: Scalar> Vector<K> {
-    fn from(data: Vec<K>) -> Self {
-        Vector { data }
+impl<K: Copy + AddAssign + SubAssign + MulAssign> Vector<K> {
+    fn from<const N: usize>(arr: [K; N]) -> Self {
+        Self { data: arr.to_vec() }
     }
 
     fn add(&mut self, v: &Vector<K>) {
-        for i in 0..self.data.len() {
-            self.data[i] = self.data[i] + v.data[i];
-        }
+        for i in 0..self.data.len() { self.data[i] += v.data[i]; }
     }
-
     fn sub(&mut self, v: &Vector<K>) {
-        for i in 0..self.data.len() {
-            self.data[i] = self.data[i] - v.data[i];
+        for i in 0..self.data.len() { self.data[i] -= v.data[i]; }
+    }
+    fn scl(&mut self, a: K) {
+        for x in self.data.iter_mut() { *x *= a; }
+    }
+}
+
+impl<K: Copy + AddAssign + SubAssign + MulAssign> Matrix<K> {
+    fn from<const R: usize, const C: usize>(arr: [[K; C]; R]) -> Self {
+        let mut cols_data = vec![vec![arr[0][0]; R]; C];
+        for r in 0..R {
+            for c in 0..C { cols_data[c][r] = arr[r][c]; }
         }
+        Matrix { data: cols_data, rows: R, cols: C }
     }
 
+    fn add(&mut self, v: &Matrix<K>) {
+        for c in 0..self.cols {
+            for r in 0..self.rows { self.data[c][r] += v.data[c][r]; }
+        }
+    }
+    fn sub(&mut self, v: &Matrix<K>) {
+        for c in 0..self.cols {
+            for r in 0..self.rows { self.data[c][r] -= v.data[c][r]; }
+        }
+    }
     fn scl(&mut self, a: K) {
-        for i in 0..self.data.len() {
-            self.data[i] = self.data[i] * a;
+        for col in self.data.iter_mut() {
+            for val in col.iter_mut() { *val *= a; }
         }
     }
 }
 
-trait Lerpable<T> {
-    fn lerp(u: &Self, v: &Self, t: T) -> Self;
+
+trait Lerpable<K> {
+    fn lerp(u: Self, v: Self, t: K) -> Self;
+}
+
+fn lerp<T, K>(u: T, v: T, t: K) -> T 
+where T: Lerpable<K> 
+{
+    T::lerp(u, v, t)
 }
 
 impl Lerpable<f32> for f32 {
-    fn lerp(u: &f32, v: &f32, t: f32) -> f32 {
-        *u + (*v - *u) * t
+    fn lerp(u: f32, v: f32, t: f32) -> f32 {
+        u + (v - u) * t
     }
 }
 
-impl<K: Scalar> Lerpable<K> for Vector<K> {
-    fn lerp(u: &Vector<K>, v: &Vector<K>, t: K) -> Vector<K> {
+impl<K: Copy + AddAssign + SubAssign + MulAssign> Lerpable<K> for Vector<K> {
+    fn lerp(u: Self, v: Self, t: K) -> Self {
         let mut res = v.clone();
-        res.sub(u);
+        res.sub(&u);
         res.scl(t);
-        res.add(u);
+        res.add(&u);
         res
     }
 }
 
-impl<K: Scalar> Lerpable<K> for Matrix<K> {
-    fn lerp(u: &Matrix<K>, v: &Matrix<K>, t: K) -> Matrix<K> {
+impl<K: Copy + AddAssign + SubAssign + MulAssign> Lerpable<K> for Matrix<K> {
+    fn lerp(u: Self, v: Self, t: K) -> Self {
         let mut res = v.clone();
-        res.sub(u);
+        res.sub(&u);
         res.scl(t);
-        res.add(u);
+        res.add(&u);
         res
     }
 }
 
-fn lerp<V, T>(u: V, v: V, t: T) -> V 
-where V: Lerpable<T> 
-{
-    V::lerp(&u, &v, t)
+
+impl<K: fmt::Display> fmt::Display for Vector<K> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, val) in self.data.iter().enumerate() {
+            write!(f, "[{}]", val)?;
+            if i < self.data.len() - 1 { writeln!(f)?; }
+        }
+        Ok(())
+    }
 }
+
+impl<K: fmt::Display + Copy> fmt::Display for Matrix<K> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[")?;
+        for r in 0..self.rows {
+            if r > 0 { write!(f, " ")?; }
+            write!(f, "[")?;
+            for c in 0..self.cols {
+                write!(f, "{}", self.data[c][r])?;
+                if c < self.cols - 1 { write!(f, ", ")?; }
+            }
+            write!(f, "]")?;
+            if r < self.rows - 1 { writeln!(f)?; }
+        }
+        write!(f, "]")
+    }
+}
+
 
 fn main() {
-    println!("Float lerp: {:?}", lerp(100f32, 4f32, 0.5f32));
+    println!("{}", lerp(0., 1., 0.));
+    println!("{}", lerp(0., 1., 1.));
+    println!("{}", lerp(0., 1., 0.5));
+    println!("{:.1}", lerp(21., 42., 0.3));
 
-    let v1 = Vector::from(vec![1, 2]);
-    let v2 = Vector::from(vec![3, 4]);
-    let vmid = lerp(v1, v2, 1);
-    println!("Vector lerp: {:?}", vmid);
+    println!("{}", lerp(Vector::from([2., 1.]), Vector::from([4., 2.]), 0.3));
 
-    let m1 = Matrix::from(vec![vec![2.0, 1.0], vec![3.0, 4.0]]);
-    let m2 = Matrix::from(vec![vec![20.0, 10.0], vec![30.0, 40.0]]);
-    let mid = lerp(m1, m2, 0.5f32);
-    println!("Matrix lerp: {:?}", mid);
+    println!("{}", lerp(
+        Matrix::from([[2., 1.], [3., 4.]]), 
+        Matrix::from([[20., 10.], [30., 40.]]), 
+        0.5
+    ));
 }
