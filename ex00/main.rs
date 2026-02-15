da82620b3e3d89a7e9d90345d12cd518546aa0ae
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-15 22:28:20 +0000

    creat Vector

--- ex00/main.rs
+++ ex00/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    ops::{Add, Sub, Mul},
-    fmt::Debug,
-};
+use std::ops::{AddAssign, SubAssign, MulAssign};
 
 #[derive(Debug, Clone)]
 struct Vector<K> {
@@ -10,84 +7,99 @@ struct Vector<K> {
 
 #[derive(Debug, Clone)]
 struct Matrix<K> {
-    data: Vec<Vec<K>>,
-    _rows: usize,
-    _cols: usize,
+    data: Vec<Vec<K>>, // Column-major
+    rows: usize,
+    cols: usize,
 }
 
-trait Scalable<S> {
-    fn scl(&mut self, a: S);
-}
-
-impl<K, S> Scalable<S> for Vector<K>
-where
-    K: Mul<Output = K> + Copy + Debug,
-    S: Into<K> + Copy, 
-{
-    fn scl(&mut self, a: S) {
-        let scalar: K = a.into();
-        for x in self.data.iter_mut() {
-            *x = *x * scalar;
-        }
+impl<K> Vector<K> {
+    pub fn size(&self) -> usize {
+        self.data.len()
     }
 }
 
-impl<K, S> Scalable<S> for Matrix<K>
-where
-    K: Mul<Output = K> + Copy + Debug,
-    S: Into<K> + Copy,
-{
-    fn scl(&mut self, a: S) {
-        let scalar: K = a.into();
-        for row in self.data.iter_mut() {
-            for x in row.iter_mut() {
-                *x = *x * scalar;
-            }
-        }
+impl<K> Matrix<K> {
+    pub fn shape(&self) -> (usize, usize) {
+        (self.rows, self.cols)
+    }
+
+    pub fn size(&self) -> usize {
+        self.rows * self.cols
+    }
+
+    pub fn is_square(&self) -> bool {
+        self.rows == self.cols
     }
 }
+
 
 impl<K> Vector<K> 
-where K: Add<Output = K> + Sub<Output = K> + Copy + Debug 
+where K: Copy + AddAssign + SubAssign + MulAssign 
 {
-    fn from<T: Into<Vec<K>>>(values: T) -> Self {
-        Vector { data: values.into() }
+    pub fn from(values: Vec<K>) -> Self {
+        Self { data: values }
     }
-    fn add(&mut self, v: &Vector<K>) {
-        for i in 0..self.data.len() { self.data[i] = self.data[i] + v.data[i]; }
+
+    pub fn add(&mut self, v: &Vector<K>) {
+        if self.size() != v.size() { return; }
+        for i in 0..self.size() { self.data[i] += v.data[i]; }
     }
-    fn sub(&mut self, v: &Vector<K>) {
-        for i in 0..self.data.len() { self.data[i] = self.data[i] - v.data[i]; }
+
+    pub fn sub(&mut self, v: &Vector<K>) {
+        if self.size() != v.size() { return; }
+        for i in 0..self.size() { self.data[i] -= v.data[i]; }
+    }
+
+    pub fn scl(&mut self, a: K) {
+        for x in self.data.iter_mut() { *x *= a; }
     }
 }
 
 impl<K> Matrix<K> 
-where K: Add<Output = K> + Sub<Output = K> + Copy + Debug 
+where K: Copy + AddAssign + SubAssign + MulAssign 
 {
-    fn from<T : Into<Vec<Vec<K>>>>(values: T) -> Self{
-        let data = values.into();
-        let _rows = data.len();
-        let _cols = if _rows > 0 { data[0].len() } else { 0 };
-        Matrix {data, _rows, _cols}
+    pub fn from(values: Vec<Vec<K>>) -> Self {
+        let cols = values.len();
+        let rows = if cols > 0 { values[0].len() } else { 0 };
+        Self { data: values, rows, cols }
     }
-    fn add(&mut self, v: &Matrix<K>){
-        for i in 0..self.data.len(){
-            for j in 0..self.data[i].len(){ self.data[i][j] = self.data[i][j] + v.data[i][j]; }
+
+    pub fn add(&mut self, v: &Matrix<K>) {
+        if self.shape() != v.shape() { return; }
+        for c in 0..self.cols {
+            for r in 0..self.rows {
+                self.data[c][r] += v.data[c][r];
+            }
         }
     }
-    fn sub(&mut self, v: &Matrix<K>){
-        for i in 0..self.data.len(){
-            for j in 0..self.data[i].len(){ self.data[i][j] = self.data[i][j] - v.data[i][j]; }
+
+    pub fn sub(&mut self, v: &Matrix<K>) {
+        if self.shape() != v.shape() { return; }
+        for c in 0..self.cols {
+            for r in 0..self.rows {
+                self.data[c][r] -= v.data[c][r];
+            }
+        }
+    }
+
+    pub fn scl(&mut self, a: K) {
+        for col in self.data.iter_mut() {
+            for val in col.iter_mut() { *val *= a; }
         }
     }
 }
 
 fn main() {
-    let mut v_float = Vector::from(vec![1.5, 2.5, 3.5]);
-    v_float.scl(2); 
-    println!("Vector f32 scaled by int 2: {:?}", v_float.data);
+    let v = Vector::from(vec![1.0, 2.0, 3.0]);
+    println!("Vector dimension (size): {}", v.size()); // 3
 
-    let mut m_int = Matrix::from(vec![vec![1, 2], vec![3, 4]]);
-    m_int.scl(10); 
-    println!("Matrix i32 scaled by int 10: {:?}", m_int.data);
+    let m = Matrix::from(vec![
+        vec![1.0, 3.0], // Column 1
+        vec![2.0, 4.0], // Column 2
+    ]);
+
+    let (rows, cols) = m.shape();
+    println!("Matrix Shape: {} rows x {} columns", rows, cols); // 2x2
+    println!("Matrix total elements: {}", m.size()); // 4
+    println!("Is matrix square? {}", m.is_square()); // true
 }
