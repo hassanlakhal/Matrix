abc9b2eb10d86bbcddbddea45a90d0ff49dc68c8
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-15 10:00:25 +0000

    fixed all this errors

--- ex00/main.rs
+++ ex00/main.rs
@@ -11,119 +11,83 @@ struct Vector<K> {
 #[derive(Debug, Clone)]
 struct Matrix<K> {
     data: Vec<Vec<K>>,
-    rows: usize,
-    cols: usize,
+    _rows: usize,
+    _cols: usize,
 }
 
-impl<K> Matrix<K>
-where
-    K: Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Copy + Debug,
-{
-    fn from<T : Into<Vec<Vec<K>>>>(values: T) -> Self{
-        let data = values.into();
-        let rows = data.len();
-        let cols = if rows > 0 { data[0].len() } else { 0 };
-        Matrix {data, rows, cols}
-    }
+trait Scalable<S> {
+    fn scl(&mut self, a: S);
+}
 
-    fn add(&mut self, v: &Matrix<K>){
-        for i in 0..self.data.len(){
-            for j in 0..self.data[i].len(){
-                self.data[i][j] = self.data[i][j] + v.data[i][j];
-            }
+impl<K, S> Scalable<S> for Vector<K>
+where
+    K: Mul<Output = K> + Copy + Debug,
+    S: Into<K> + Copy, 
+{
+    fn scl(&mut self, a: S) {
+        let scalar: K = a.into();
+        for x in self.data.iter_mut() {
+            *x = *x * scalar;
         }
     }
-    fn sub(&mut self, v: &Matrix<K>){
-        for i in 0..self.data.len(){
-            for j in 0..self.data[i].len(){
-                self.data[i][j] = self.data[i][j] - v.data[i][j];
-            }
-        }
-    }
-    fn scl(&mut self, a: K){
-        for i in 0..self.data.len(){
-            for j in 0..self.data[i].len(){
-                self.data[i][j] = self.data[i][j] * a;
+}
+
+impl<K, S> Scalable<S> for Matrix<K>
+where
+    K: Mul<Output = K> + Copy + Debug,
+    S: Into<K> + Copy,
+{
+    fn scl(&mut self, a: S) {
+        let scalar: K = a.into();
+        for row in self.data.iter_mut() {
+            for x in row.iter_mut() {
+                *x = *x * scalar;
             }
         }
     }
 }
 
 impl<K> Vector<K> 
-where
-    K: Add<Output = K> + Sub<Output = K> + Mul<Output = K> + Copy + Debug,
+where K: Add<Output = K> + Sub<Output = K> + Copy + Debug 
 {
-        fn from<T: Into<Vec<K>>>(values: T) -> Self {
-            Vector { data: values.into() }
-        }
+    fn from<T: Into<Vec<K>>>(values: T) -> Self {
+        Vector { data: values.into() }
+    }
+    fn add(&mut self, v: &Vector<K>) {
+        for i in 0..self.data.len() { self.data[i] = self.data[i] + v.data[i]; }
+    }
+    fn sub(&mut self, v: &Vector<K>) {
+        for i in 0..self.data.len() { self.data[i] = self.data[i] - v.data[i]; }
+    }
+}
 
-        fn size(&self) -> usize {
-            self.data.len()
+impl<K> Matrix<K> 
+where K: Add<Output = K> + Sub<Output = K> + Copy + Debug 
+{
+    fn from<T : Into<Vec<Vec<K>>>>(values: T) -> Self{
+        let data = values.into();
+        let _rows = data.len();
+        let _cols = if _rows > 0 { data[0].len() } else { 0 };
+        Matrix {data, _rows, _cols}
+    }
+    fn add(&mut self, v: &Matrix<K>){
+        for i in 0..self.data.len(){
+            for j in 0..self.data[i].len(){ self.data[i][j] = self.data[i][j] + v.data[i][j]; }
         }
-
-        fn print(&self) {
-            println!("{:?}", self.data);
+    }
+    fn sub(&mut self, v: &Matrix<K>){
+        for i in 0..self.data.len(){
+            for j in 0..self.data[i].len(){ self.data[i][j] = self.data[i][j] - v.data[i][j]; }
         }
-
-        fn add(&mut self, v: &Vector<K>) {
-            if self.size() != v.size() {
-                eprintln!("Error: Vectors must have the same size");
-                return;
-            }
-            for i in 0..self.data.len() {
-                self.data[i] = self.data[i] + v.data[i];
-            }
-        }
-
-        fn sub(&mut self, v: &Vector<K>){
-            if self.size() != v.size() {
-                eprintln!("Error: Vectors must have the same size");
-                return;
-            }
-            for i in 0..self.data.len() {
-                self.data[i] = self.data[i] - v.data[i];
-            }
-        }
-        fn scl(&mut self, a: K){
-            for i in 0..self.data.len(){
-                self.data[i] = self.data[i] * a;
-            }
-        }
+    }
 }
 
 fn main() {
-    let mut u = Vector::from([2., 3., 5.]);
-    let v = Vector::from([5., 7., -100.]);
-    let mut j = Vector::from([8., 9.]);
+    let mut v_float = Vector::from(vec![1.5, 2.5, 3.5]);
+    v_float.scl(2); 
+    println!("Vector f32 scaled by int 2: {:?}", v_float.data);
 
-    println!("Before add:");
-    u.print();  
-
-    u.add(&v);
-
-
-    println!("After add:");
-    u.print();
-
-    j.sub(&v);
-    println!("After sub:");
-    j.print();
-    j.scl(5.);
-    j.print();
-
-
-    let mut u1 = Matrix::from(vec![vec![1., 2.], vec![3., 4.]]);
-    let mut u2 = Matrix::from(vec![vec![1., 0.], vec![8., 7.]]);
-    let mut u3 = Matrix::from(vec![vec![1., 0.], vec![8., 7.]]);
-    let v1 = Matrix::from(vec![vec![7., 4.], vec![-2., 2.]]);
-    u1.add(&v1);
-    u2.sub(&v1);
-    u3.scl(-1.);
-    println!("Rows: {}, Cols: {}", u1.rows, u1.cols);
-    println!("Data: {:?}", u1.data);
-    println!("=================");
-    println!("Data: {:?}", u2.data);
-    println!("=================");
-    println!("Data: {:?}", u3.data);
-
+    let mut m_int = Matrix::from(vec![vec![1, 2], vec![3, 4]]);
+    m_int.scl(10); 
+    println!("Matrix i32 scaled by int 10: {:?}", m_int.data);
 }
