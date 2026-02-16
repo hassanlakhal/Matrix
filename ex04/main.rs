6c04a6a261f02219f532ed4c37e6fd00e5ca6381
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-16 22:23:42 +0000

    fixed this

--- ex04/main.rs
+++ ex04/main.rs
@@ -20,26 +20,38 @@ where K: Copy + AddAssign + SubAssign + MulAssign
         Self { data: values }
     }
 
-    fn norm_1(&mut self) -> f32{
-
+    fn norm_1(&mut self) -> K
+    where 
+        K : Add<Output = K> + Mul<Output = K> + Default 
+    {
+        let mut res = K::default();
+        let mut vec = self.data.clone();
+        for i in 0..vec.len(){
+            if vec[i] < 0.{
+                vec[i] = vec[i] * -1.;
+            }
+            res += vec[i];
+        }
+        res
     }
-    fn norm(&mut self) -> f32{
+    // fn norm(&mut self) -> K{
 
-    }
-    fn norm_inf(&mut self) -> f32{
+    // }
+    // fn norm_inf(&mut self) -> K{
 
-    }
+    // }
 }
 
 
 fn main() {
-    let u = Vector::from(vec![0., 0., 0.]);
-    println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
-    // 0.0, 0.0, 0.0
-    let u = Vector::from(vec![1., 2., 3.]);
-    println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
-    // 6.0, 3.74165738, 3.0
-    let u = Vector::from(vec![-1., -2.]);
-    println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    let mut u = Vector::from(vec![0., 0., 0.]);
+    println!("{:?}",u.norm_1());
+    // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    // // 0.0, 0.0, 0.0
+    // let u = Vector::from(vec![1., 2., 3.]);
+    // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    // // 6.0, 3.74165738, 3.0
+    // let u = Vector::from(vec![-1., -2.]);
+    // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
     // 3.0, 2.236067977, 2.0
 }
