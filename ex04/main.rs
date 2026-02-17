a2be1e91e126d51f194b6861b57c72199b161672
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-17 03:13:17 +0000

    ex04

--- ex04/main.rs
+++ ex04/main.rs
@@ -13,6 +13,26 @@ impl<K> Vector<K> {
     }
 }
 
+trait MathOps{
+    fn mul_add(self, a: Self, b: Self) -> Self;
+    fn pow(self, exp: u32) -> Self;
+    fn max(self, other: Self) -> Self; 
+}
+
+impl MathOps for f32 {
+    fn mul_add(self, a: Self, b: Self) -> Self {
+        f32::mul_add(self, a, b)
+    }
+
+    fn pow(self, exp: u32) -> Self {
+        f32::powf(self, exp as f32) as f32
+    }
+
+    fn max(self, other: Self) -> Self {
+        f32::max(self, other)
+    }
+}
+
 
 impl<K> Vector<K> 
 where K: Copy + AddAssign + SubAssign + MulAssign + PartialOrd
@@ -36,9 +56,19 @@ where K: Copy + AddAssign + SubAssign + MulAssign + PartialOrd
         }
         res
     }
-    // fn norm(&mut self) -> K{
-
-    // }
+    fn norm(&mut self) -> K
+    where
+        K: Add<Output = K> + Mul<Output = K> + MathOps + Default
+    {
+        let vec = self.data.clone();
+        let mut res = K::default();
+        for i in 0..self.size(){
+            
+            res = res + vec[i].pow(2);
+        }
+        res = res.pow(1.5);
+        res
+    }
     // fn norm_inf(&mut self) -> K{
 
     // }
@@ -47,15 +77,15 @@ where K: Copy + AddAssign + SubAssign + MulAssign + PartialOrd
 
 fn main() {
     let mut u = Vector::from(vec![0., 0., 0.]);
-    println!("{:?}",u.norm_1());
+    println!("{:?}",u.norm());
     // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
     // // 0.0, 0.0, 0.0
     let mut u = Vector::from(vec![1., 2., 3.]);
     // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
-    println!("{:?}", u.norm_1());
+    println!("{:?}", u.norm());
     // // 6.0, 3.74165738, 3.0
     let mut u = Vector::from(vec![-1., -2.]);
-    println!("{:?}", u.norm_1());
+    println!("{:?}", u.norm());
     // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
     // 3.0, 2.236067977, 2.0
 }
