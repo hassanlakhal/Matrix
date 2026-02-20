7435ab7d960f01ce70f06eed4c2002d7634be489
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-02-20 00:06:51 +0000

    fixed ex06

--- ex06/main.rs
+++ ex06/main.rs
@@ -1,97 +1,85 @@
-use std::ops::{AddAssign, SubAssign, MulAssign,DivAssign, Mul, Add, Div, Sub};
-use std::cmp::{PartialOrd};
-use std::fmt::{Display};
+use std::ops::{
+    Add, Sub, Mul, Div,
+    AddAssign, SubAssign, MulAssign, DivAssign,
+    Index, IndexMut
+};
+use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug, Clone)]
 struct Vector<K> {
     data: Vec<K>,
 }
 
-pub trait KField: 
+pub trait KField:
     Copy + Default + PartialEq + Display +
-    Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> +
-    AddAssign + SubAssign + MulAssign + DivAssign 
+    Add<Output = Self> + Sub<Output = Self> +
+    Mul<Output = Self> + Div<Output = Self> +
+    AddAssign + SubAssign + MulAssign + DivAssign
 {}
 
 impl KField for f32 {}
-
 
 impl<K: KField> Vector<K> {
     pub fn from<const N: usize>(arr: [K; N]) -> Self {
         Self { data: arr.to_vec() }
     }
-}
 
-trait MathOps{
-    fn pow(self, exp: u32) -> Self;
-    fn powf(self, exp: f32) -> Self;
-}
-
-impl MathOps for f32 {
-    
-    fn pow(self, exp: u32) -> Self {
-        f32::powf(self, exp as f32) as f32
+    fn size(&self) -> usize {
+        self.data.len()
     }
-
-    fn powf(self, exp: f32) -> Self {
-        f32::powf(self, exp as f32) as f32
-    }
-
 }
 
+impl<K> Index<usize> for Vector<K> {
+    type Output = K;
 
-impl<K> Vector<K> 
-where K: Copy + AddAssign + SubAssign + MulAssign + PartialOrd
-{
-    fn size(&self) -> usize
-    where
-        K: Add<Output = K> + Mul<Output = K> + MathOps + Default
-    {
-        let size; 
-        size = self.data.len();
-        size
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index]
     }
-
 }
 
-fn cross_product<K, KField>(u: &Vector::<K>, v: &Vector::<K>) -> Vector<K>
+impl<K> IndexMut<usize> for Vector<K> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data[index]
+    }
+}
+
+impl<K: Display> Display for Vector<K> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        for val in &self.data {
+            writeln!(f, "[{}]", val)?;
+        }
+        Ok(())
+    }
+}
+
+fn cross_product<K>(u: &Vector<K>, v: &Vector<K>) -> Vector<K>
 where
-    K: Copy + AddAssign + SubAssign + MulAssign + PartialOrd + Default + MathOps
-     + Mul<Output = K> + Add<Output = K> + Div<Output = K> + Into<f32> 
-
+    K: KField
 {
-
-    if u.size() != 3. && v.size() != 3{
-        return Vector::from([0., 0., 0.]);
+    if u.size() != 3 || v.size() != 3 {
+        let zero = K::default();
+        return Vector::from([zero, zero, zero]);
     }
 
-    let mut cross_v = Vector::from([0., 0. , 0.]);
+    let mut result = Vector::from([K::default(); 3]);
 
-    cross_v[0] = u[1] * v[2] - u[2] * v[1];
-    cross_v[1] = u[2] - v[0] - u[0] * v[2];
-    cross_v[2] = u[0] - v[1] - u[1] * v[0];
+    result[0] = u[1] * v[2] - u[2] * v[1];
+    result[1] = u[2] * v[0] - u[0] * v[2];
+    result[2] = u[0] * v[1] - u[1] * v[0];
 
-    cross_v
+    result
 }
-fn main(){
 
+fn main() {
     let u = Vector::from([0., 0., 1.]);
     let v = Vector::from([1., 0., 0.]);
     println!("{}", cross_product(&u, &v));
-    // [0.]
-    // [1.]
-    // [0.]
+
     let u = Vector::from([1., 2., 3.]);
     let v = Vector::from([4., 5., 6.]);
     println!("{}", cross_product(&u, &v));
-    // [-3.]
-    // [6.]
-    // [-3.]
+
     let u = Vector::from([4., 2., -3.]);
     let v = Vector::from([-2., -5., 16.]);
     println!("{}", cross_product(&u, &v));
-    // [17.]
-    // [-58.]
-    // [-16.]
-
 }
