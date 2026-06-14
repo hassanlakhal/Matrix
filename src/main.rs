6dc210a9ae9f3ef029db9d0a3343f3a4c92bc278
Hassan Lakhal <hlakhal.contact@gmail.com>
2026-06-14 23:27:11 +0100

    add linear_combination vector

--- src/main.rs
+++ src/main.rs
@@ -69,11 +69,11 @@ fn main(){
     println!("{}", lerp(21., 42., 0.3));
     // 27.3
 
-    // println!("{}", lerp(Vector::from([2., 1.]), Vector::from([4., 2.]), 0.3));
+    println!("{}", lerp(Vector::from([2., 1.]), Vector::from([4., 2.]), 0.3));
     // // [2.6]
     // // [1.3]
-    // println!("{}", lerp(Matrix::from([[2., 1.], [3., 4.]]), Matrix::from([[20.,
-    // 10.], [30., 40.]]), 0.5));
+    println!("{}", lerp(Matrix::from([[2., 1.], [3., 4.]]), Matrix::from([[20.,
+    10.], [30., 40.]]), 0.5));
     // [[11., 5.5]
     // [16.5, 22.]]
 
--- src/types/lerp.rs
+++ src/types/lerp.rs
@@ -1,5 +1,6 @@
-// use super::field::Field;
-// use std::fmt;
+use super::field::Field;
+use crate::Matrix;
+use crate::Vector;
 
 pub trait Lerp:{
     fn lerp(u: Self, v: Self, t: f32) -> Self;
@@ -9,6 +10,30 @@ impl Lerp for f32{
     fn lerp(u: Self, v: Self, t: f32) -> Self{
         f32::mul_add(t, v - u, u)
     }
+}
+
+impl<K: Field + std::convert::From<f32>> Lerp for Matrix<K>{
+    fn lerp(u: Self, mut v: Self, t: f32) -> Self{
+
+        let t_generic: K = t.into();
+        v.sub(u.clone());
+        v.scl(t_generic);
+        v.add(u.clone());
+        v
+    }
+
+}
+
+impl<K: Field + std::convert::From<f32>> Lerp for Vector<K>{
+    fn lerp(u: Self, mut v: Self, t: f32) -> Self{
+        
+        let t_generic: K = t.into();
+        v.sub(u.clone());
+        v.scl(t_generic);
+        v.add(u.clone());
+        v
+    }
+
 }
 
 pub fn lerp<V: Lerp>(u: V, v: V, t: f32) -> V {
